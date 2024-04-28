dd840bf6e006333150da6c2db45a0bd959c42bdf
MRAzaly20 <muhammadrazalyy@gmail.com>
2024-04-28 12:57:14 +0700

    initial commit

---
+++ cmd.sh
@@ -0,0 +1,7 @@
+echo "# periodic-table" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MRAzaly20/periodic-table.git
+git push -u origin main
